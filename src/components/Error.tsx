5252f9633dcbd1009fd9b41d76e9140a6bcb812c
Utkarsh 2700 <utkarshsharma2718@gmail.com>
2024-12-10 22:24:33 +0530

    fix typescript issues

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,5 +1,10 @@
 // import React from "react";
+interface ErrorMessageProps {
+  message: string;
+}
 
-const Error = ({ message }) => <div>{message}</div>;
+const Error: React.FC<ErrorMessageProps> = ({ message }) => (
+  <div>{message}</div>
+);
 
 export default Error;
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,4 +1,48 @@
-const RecipeCard = ({ recipe }) => (
+interface Recipe {
+  vegetarian: boolean;
+  vegan: boolean;
+  glutenFree: boolean;
+  dairyFree: boolean;
+  veryHealthy: boolean;
+  cheap: boolean;
+  veryPopular: boolean;
+  sustainable: boolean;
+  lowFodmap: boolean;
+  weightWatcherSmartPoints: number;
+  gaps: string;
+  preparationMinutes: number | null;
+  cookingMinutes: number | null;
+  aggregateLikes: number;
+  healthScore: number;
+  creditsText: string;
+  license: string;
+  sourceName: string;
+  pricePerServing: number;
+  extendedIngredients: ExtendedIngredient[];
+  id: number;
+  title: string;
+  readyInMinutes: number;
+  servings: number;
+  sourceUrl: string;
+  image: string;
+  imageType: string;
+  summary: string;
+  cuisines: string[];
+  dishTypes: string[];
+  diets: string[];
+  occasions: string[];
+  instructions: string;
+  analyzedInstructions: AnalyzedInstruction[];
+  originalId: string | null;
+  spoonacularScore: number;
+  spoonacularSourceUrl: string;
+}
+
+interface RecipeCardProps {
+  recipe: Recipe;
+}
+
+const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => (
   <div className="recipe-card shadow bg-[#EAFFDA] h-[350px] rounded-md pt-4 flex-shrink-0">
     <img src={recipe.image} alt={recipe.title} />
     <h2 className="text-[18px] leading-[22px] font-bold">{recipe.title}</h2>
